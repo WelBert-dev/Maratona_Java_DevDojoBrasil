492113a10deeff7c000d47a313e8c7e3a24603d5
WelBert-dev <wellison.bertelli@hotmail.com>
2022-12-07 15:06:36 -0300

    First

---
+++ src/date/Aula112Date.java
@@ -0,0 +1,19 @@
+package date;
+
+// A classe Date sofreu algumas mudanças ao longo do tempo
+// pois de inicio não supria 100% das necessidades,
+// então o java creiou outras classes com o mesmo contexto
+// para tentar suprir essa carencia
+// Cronograma:
+// Date -> Calendar -> Pacote inteiro para lidar com datas.
+
+// Date: Muitos sistemas legados ainda utilizam ela (java.util):
+
+import java.util.Date;
+
+public class Aula112Date {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+    }
+}
