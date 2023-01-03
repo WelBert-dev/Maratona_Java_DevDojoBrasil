24378e2bde33a3e3d9cd245f64cfedae12ec03ee
WelBert-dev <wellison.bertelli@hotmail.com>
2023-01-03 20:08:26 -0300

    add mais sobre quantificadores, Aula135

--- src/regex/Aula134PatternAndMatcher_quantificadores.java
+++ src/regex/Aula134PatternAndMatcher_quantificadores.java
@@ -1,11 +1,13 @@
 package regex;
 
+// Mais informações: https://www.programacaoprogressiva.net/2012/08/java-expressoes-regulares-regex-em-java_2423.html
 // Utilizamos quantificadores quando desejamos retornar vários valores de um range "[]"
 // pois o default é retornar apenas uma ocorrência (a primeira), quando queremos
 // continuar a interação sobre esse range e retornar mais valores utilizamos um
 // agrupamento "([])" seguido de um dos operadores descritos abaixo "([])*";
 
 // Operadores quantificador:
+// . substitui qualquer caractere
 // ? Zero ou uma
 // * Zero ou mais
 // + Uma ou mais
@@ -14,6 +16,7 @@ package regex;
 // | ou/or (exemplo: o(v|c)o ovo ou oco
 // $ fim da linha
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,6 +41,41 @@ public class Aula134PatternAndMatcher_quantificadores {
             // Índice do primeiro char: 9 | Valor encontrado: 0xFFABC
             // Índice do primeiro char: 17 | Valor encontrado: 0x109
             // Índice do primeiro char: 23 | Valor encontrado: 0x1
+
+        }
+    }
+}
+
+class Aula135PatternAndMatcher_quantificadorespt02 {
+    public static void main(String[] args) {
+        // exemplo: regex para filtrar por emails válidos:
+
+        String regex = "([a-zA-Z0-9\\.-_])+@([a-zA-Z])+(\\.([a-zA-Z])+)+";
+        String texto = "wellison.bertelli@hotmail.com, bertelli.wesley@gmail.com, danielle@uoul.com.br, pamonha@gmail";
+
+        // Classes utilitárias:
+        // Para Strings é possivel verificar se ela faz match com uma regex:
+        System.out.println("$%@hotmail.com".matches(regex));
+        System.out.println("wellison@hotmail.com".matches(regex));
+
+        // o método .split() utiliza uma regex:
+        System.out.println(Arrays.toString(texto.split(",")));
+        System.out.println(texto.split(",")[1].trim());
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto: "+ texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex: "+ regex);
+        System.out.println("Posições encontradas: ");
+
+        while(matcher.find()) {
+            System.out.println("Índice do primeiro char: "+ matcher.start()+
+                    " | Valor encontrado: "+matcher.group());
+            // Posições encontradas:
+            // Índice do primeiro char: 0 | Valor encontrado: wellison.bertelli@hotmail.com
+            // Índice do primeiro char: 31 | Valor encontrado: bertelli.wesley@gmail.com
+            // Índice do primeiro char: 58 | Valor encontrado: danielle@uoul.com.br
 
         }
     }
