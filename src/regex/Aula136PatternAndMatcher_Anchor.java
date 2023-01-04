5846dedd1c2945f4517e7a8362637f7630711f87
WelBert-dev <wellison.bertelli@hotmail.com>
2023-01-03 23:05:40 -0300

    add apenas explicações do Anchor "^" e site regexr

---
+++ src/regex/Aula136PatternAndMatcher_Anchor.java
@@ -0,0 +1,10 @@
+package regex;
+
+// Site bom para treinar regex: https://regexr.com/
+// O Anchor "^" possuí duas utilidades sendo elas:
+// Utilizando ele no começo da expressão fora dos "[]" indicamos que queremos EXATAMENTE a próxima expressão
+// isto em referência ao inicio do texto no qual estamos aplicando o match do regex.
+// Utilizando ele dentro dos "[^\w]" indicamos que NÂO queremos esse valor, ou seja tudo menos ele.
+
+public class Aula136PatternAndMatcher_Anchor {
+}
