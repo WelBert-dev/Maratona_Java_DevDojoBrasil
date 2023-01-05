ca369b5a98d12d2e2ce2f6675ee6a87738bfe9e6
WelBert-dev <wellison.bertelli@hotmail.com>
2023-01-04 21:26:53 -0300

    add classe File base p/ manip de arquivos simples

---
+++ src/io/Aula138File_introduction_Utils.java
@@ -0,0 +1,52 @@
+package io;
+
+// Todas classes especializadas em input e output utilizam como base a
+// classe File, pois para utilizar as outras deste pacote é necessário uma instância dela.
+
+// Classes aboradads neste package:
+// File
+// FileWriter
+// FileReader
+// BufferedWriter (Em memória, logo == + rápido)
+// BufferedReader (Em memória, logo == + rápido)
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Map;
+
+public class Aula138File_introduction_Utils {
+    public static void main(String[] args) {
+        File file = new File("/home/welbert/Documentos/github/MaratonaJava-DevDojo/file.txt"); // caminho absoluto ou parcial
+        boolean isDeleted;
+
+        if (file.exists()) {
+           // isDeleted = file.delete();
+        }
+
+        System.out.println(file.isFile()); // true
+        System.out.println(file.isDirectory()); // false
+        System.out.println(file.isHidden()); // false
+
+        System.out.println(file.getAbsolutePath()); // /home/welbert/Documentos/github/MaratonaJava-DevDojo/file.txt
+        System.out.println(file.getPath()); // /home/welbert/Documentos/github/MaratonaJava-DevDojo/file.txt
+        System.out.println(file.getParent()); // /home/welbert/Documentos/github/MaratonaJava-DevDojo
+        System.out.println(file.getTotalSpace()); // 318450962432
+        System.out.println(file.getFreeSpace()); // 254839607296
+
+        Map<String, String> shortIds = ZoneId.SHORT_IDS;
+        for (String keyOfZone  : shortIds.keySet()) {
+            System.out.println("Chave: " + keyOfZone +  " | Valor: "+shortIds.get(keyOfZone));
+        }
+
+        // Ultima modificação em LocalDateTime
+        System.out.println(Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.of("America/Sao_Paulo")).toLocalDateTime());
+        // 2023-01-04T21:08:13.624
+        try {
+            boolean isCreated = file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
