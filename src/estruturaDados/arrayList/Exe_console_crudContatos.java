e018fd337d8e33a3efb52d8f4798c7268e2aec2a
WelBert-dev <wellison.bertelli@hotmail.com>
2023-01-17 00:52:08 -0300

    add operação 2 e 3

--- src/estruturaDados/arrayList/Exe_console_crudContatos.java
+++ src/estruturaDados/arrayList/Exe_console_crudContatos.java
@@ -1,6 +1,7 @@
 package estruturaDados.arrayList;
 
 import estruturaDados.models.ContatoModel;
+import estruturaDados.utils.ClearConsole;
 
 import java.util.Scanner;
 
@@ -20,6 +21,7 @@ public class Exe_console_crudContatos {
                     option01AddContactInTheLast(scan, contatos);
                     break;
                 case 2:
+                    option02AddContactToIndex(scan, contatos);
                     break;
             }
         }
@@ -80,6 +82,8 @@ public class Exe_console_crudContatos {
     // opções do menu:
 
     public static void option01AddContactInTheLast(Scanner scan, VetorGenericoComGenerics<ContatoModel> contatos ) {
+        ClearConsole.clear();
+
         boolean isFinalized = false;
         VetorGenericoComGenerics<ContatoModel> newContacts = new VetorGenericoComGenerics<ContatoModel>(3);
 
@@ -106,7 +110,7 @@ public class Exe_console_crudContatos {
 
                 getLinha();
                 for (int i = 0; i <= newContacts.getSize()-1; i++) {
-                    System.out.println("------------ [Contato " + i+1 + "] --------------");
+                    System.out.println("------------ [Contato " + (i+1) + "] --------------");
                     System.out.println("Nome: " + newContacts.getElement(i).getNome());
                     System.out.println("Email: " + newContacts.getElement(i).getEmail());
                     System.out.println("Telefone: " + newContacts.getElement(i).getTelefone());
@@ -118,7 +122,7 @@ public class Exe_console_crudContatos {
                     resp = readInformation("\n[Operação INVÀLIDA!! ;-;]\n[OK] - Confirmar cadastro.\n[C] - Cancelar operção\n R: ", scan);
                 }
 
-                if (resp.equals("OK")) {
+                if (resp.toUpperCase().equals("OK")) {
                     ContatoModel contato;
                     for (int i = 0; i <= newContacts.getSize()-1; i++) {
                         contato = new ContatoModel(newContacts.getElement(i).getNome(),
@@ -130,12 +134,93 @@ public class Exe_console_crudContatos {
                 } else {
                     newContacts.clear();
                 }
+
+                System.out.println(contatos);
             }
         }
     }
 
     public static void option02AddContactToIndex(Scanner scan, VetorGenericoComGenerics<ContatoModel> contatos ) {
+        boolean isFinalized = false;
 
+        getLinha();
+        System.out.println("\n[Cadastro de contato em uma posição especifica]\n");
+
+        System.out.println("------------ [Lista atual: ] --------------");
+
+        for (int i = 0; i <= contatos.getSize()-1; i++) {
+            System.out.println("------------ [Contato do índice" + i + "] --------------");
+            System.out.println("Nome: " + contatos.getElement(i).getNome());
+            System.out.println("Email: " + contatos.getElement(i).getEmail());
+            System.out.println("Telefone: " + contatos.getElement(i).getTelefone());
+        }
+
+        getLinha();
+
+        System.out.println("-------------- [Novo contato] --------------");
+
+        while(!isFinalized) {
+            getLinha();
+
+            String name = readInformation("Nome: ", scan);
+            String email = readInformation("Email: ", scan);
+            String numero = readInformation("Telefone: ", scan);
+            int index = readInformationInt("Índice: ", scan);
+
+            if (!contatos.add(new ContatoModel(name, email, numero), index))
+                System.out.println("Ocorreu um erro ao tentar adicionar, deseja continuar operações?");
+
+            String resp = readInformation("Adicionar mais? [S] - Sim [N] - Não\n R: ", scan);
+
+            while(!resp.isEmpty() && !resp.toUpperCase().equals("S") && !resp.toUpperCase().equals("N") && !resp.toUpperCase().equals("NÃO") && !resp.toUpperCase().equals("SIM")) {
+                resp = readInformation("Adicionar mais? [S] - Sim [N] - Não\n R: ", scan);
+            }
+
+            if (resp.toUpperCase().equals("N") || resp.toUpperCase().equals("NÃO")) {
+                isFinalized = true;
+
+                getLinha();
+
+                System.out.println("------------ [Lista nova: ] --------------");
+                printContactList(contatos);
+
+                getLinha();
+            }
+        }
+    }
+
+    public static void option03getContactWithIndex(Scanner scan, VetorGenericoComGenerics<ContatoModel> contatos ) {
+        boolean isFinalized = false;
+
+        getLinha();
+        System.out.println("\n[Obter um contato utilizando um index como flag]\n");
+
+        while(!isFinalized) {
+            getLinha();
+
+            String resp = readInformation("Qual índice utilizar na consulta?\n R: ", scan);
+
+            try {
+                ContatoModel contato = contatos.getElement(Integer.parseInt(resp));
+                System.out.println("[Contato encontrado, segue informações:]");
+                printAtributesAndValuesFromContact(contato);
+
+                resp = readInformation("\n R:", scan);
+
+            } catch(Exception ex) {
+                System.out.println("Elemento não encontrado, tente outro índice!! ;-;");
+            }
+
+            if (resp.toUpperCase().equals("N") || resp.toUpperCase().equals("NÃO")) {
+                isFinalized = true;
+
+                getLinha();
+                System.out.println("------------ [Lista nova: ] --------------");
+                getLinha();
+
+                printContactList(contatos);
+            }
+        }
     }
 
     public static String readInformation(String msg, Scanner scan) {
@@ -145,7 +230,7 @@ public class Exe_console_crudContatos {
 
     public static int readInformationInt(String msg, Scanner scan) {
         System.out.print(msg);
-        int resp;
+        int resp = -1;
         boolean isValid = false;
 
         while(!isValid) {
@@ -158,7 +243,20 @@ public class Exe_console_crudContatos {
             }
         }
 
-        return Integer.parseInt(scan.nextLine());
+        return resp;
+    }
+
+    public static void printContactList(VetorGenericoComGenerics<ContatoModel> contatos) {
+        for (int i = 0; i <= contatos.getSize()-1; i++) {
+            System.out.println("------------ [Contato do índice" + i + "] --------------");
+            printAtributesAndValuesFromContact(contatos.getElement(i));
+        }
+    }
+
+    public static void printAtributesAndValuesFromContact(ContatoModel contato) {
+        System.out.println("Nome: " + contato.getNome());
+        System.out.println("Email: " + contato.getEmail());
+        System.out.println("Telefone: " + contato.getTelefone());
     }
 
     public static void getLinha() {
---
+++ src/estruturaDados/utils/ClearConsole.java
@@ -0,0 +1,23 @@
+package estruturaDados.utils;
+
+public class ClearConsole {
+
+    public static void clear(){
+
+        try{
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")){
+                Runtime.getRuntime().exec("cls");
+                System.out.println("Entrou no if");
+
+            }else{
+                Runtime.getRuntime().exec("clear");
+                System.out.println("Entrou no else");
+            }
+        }
+        catch (final Exception e){
+            //  Tratar Exceptions
+        }
+    }
+}
