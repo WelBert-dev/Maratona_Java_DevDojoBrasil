a8a288ecdc66a514f7e3aaf4c9daa088f6f12e8d
WelBert-dev <wellison.bertelli@hotmail.com>
2023-01-30 20:48:39 -0300

    Finaliza desafio 1 impressão de documentos

---
+++ src/estruturaDados/models/DocumentModel.java
@@ -0,0 +1,30 @@
+package estruturaDados.models;
+
+public class DocumentModel {
+
+    public DocumentModel() {}
+
+    public DocumentModel(String nome, int paperNum) {
+        this.nome = nome;
+        this.paperNum = paperNum;
+    }
+
+    private String nome;
+    private int paperNum;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPaperNum() {
+        return paperNum;
+    }
+
+    public void setPaperNum(int paperNum) {
+        this.paperNum = paperNum;
+    }
+}
---
+++ src/estruturaDados/queue/Exe01_impressaoDocumentos.java
@@ -0,0 +1,47 @@
+package estruturaDados.queue;
+
+// Utilizando a classe fila criada anteriormente,
+// crie uma fila de Documentos que precisam ser impressos.
+// Cada Documento possuí:
+// Nome, e Quantidade de Folhas a serem impressas.
+// O Programa deve:
+// - Enfileirar os Documentos.
+// - E Seguindo a ordem o programa deve imprimir cada documento,
+// desemfileirando da fila,
+
+import estruturaDados.models.DocumentModel;
+
+public class Exe01_impressaoDocumentos {
+
+    public static void main(String[] args) {
+        // Fila sem prioridade, model não implementa o comparable
+        Queue<DocumentModel> documentQueue = new Queue<>();
+        documentQueue.enqueue(new DocumentModel("A", 1));
+        documentQueue.enqueue(new DocumentModel("A", 3));
+        documentQueue.enqueue(new DocumentModel("A", 6));
+        documentQueue.enqueue(new DocumentModel("A", 8));
+        documentQueue.enqueue(new DocumentModel("A", 2));
+
+        System.out.println(documentQueue);
+
+        int paperCount = 0;
+        while (!documentQueue.isEmpty()) {
+
+            DocumentModel doc = documentQueue.dequeue();
+
+            System.out.println("Imprimindo o documento "+ doc.getNome() +
+                    " com " + doc.getPaperNum() + " número de folhas!");
+
+            try {
+                Thread.sleep(200 * doc.getPaperNum());
+            } catch(InterruptedException ex){
+                ex.printStackTrace();
+            }
+
+            paperCount += doc.getPaperNum();
+        }
+
+        System.out.println("Todos os documentos foram impressos! ;D total de "+paperCount+" folhas");
+
+    }
+}
