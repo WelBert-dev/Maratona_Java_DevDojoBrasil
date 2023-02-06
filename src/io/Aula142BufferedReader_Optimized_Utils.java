e9ffaeb4fe845978895b72c056660303fc365c0e
WelBert-dev <wellison.bertelli@hotmail.com>
2023-02-06 20:05:52 -0300

    Finaliza io simples (old) pronto para nio (new)

---
+++ src/io/Aula142BufferedReader_Optimized_Utils.java
@@ -0,0 +1,31 @@
+package io;
+
+// Equivalente da classe FileReader porém otimizada com maior performance,
+// pois ela trabalha primeiro em Memória RAM e ao final do processamento
+// realiza a Leitura do arquivo no HD.
+// Obs: Mesmo assim ainda é necessário uma instância de FileReader
+// e passa-la para o BufferedReader.
+
+// Obs2: Ao invés de lermos caractere a caractere, ja lemos uma linha por completo
+// assim ganhamos mais performance.
+
+import java.io.*;
+
+public class Aula142BufferedReader_Optimized_Utils {
+    public static void main(String[] args) {
+        File file = new File("/home/welbert/Documentos/github/MaratonaJava-DevDojo/file.txt"); // caminho absoluto ou parcial
+
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)){
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/io/Aula143FileParaDiretorios.java
@@ -0,0 +1,32 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Aula143FileParaDiretorios {
+    public static void main(String[] args) {
+
+        try {
+            File fileDirectory = new File("pasta");
+            boolean isFolderCreated = fileDirectory.mkdir();
+            System.out.println(isFolderCreated);
+
+            File fileArchiveDirectory = new File(fileDirectory, "arquivo.txt");
+            boolean isArchiveCreated = fileArchiveDirectory.createNewFile();
+            System.out.println(isArchiveCreated);
+
+            // renomeando um arquivo:
+            File fileRenamed = new File(fileDirectory, "arquivo_renamedpa.txt"); // sempre passando o caminho correto.
+            boolean isRenamedArchive = fileArchiveDirectory.renameTo(fileRenamed);
+            System.out.println(isRenamedArchive);
+
+            // renomeando um diretório:
+            File fileFolderRenamed = new File("pasta_renamed"); // sempre passando o caminho correto.
+            boolean isRenamedFolder = fileDirectory.renameTo(fileFolderRenamed);
+            System.out.println(isRenamedFolder);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
