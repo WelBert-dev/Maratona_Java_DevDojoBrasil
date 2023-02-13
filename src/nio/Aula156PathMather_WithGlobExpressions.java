5bbda4965c2e58be7917bebf66b1ea726a11ccca
WelBert-dev <wellison.bertelli@hotmail.com>
2023-02-13 16:28:15 -0300

    write output in file and sorted in other file

--- src/nio/Aula156PathMather_WithGlobExpressions.java
+++ src/nio/Aula156PathMather_WithGlobExpressions.java
@@ -6,11 +6,29 @@ package nio;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.List;
 
 public class Aula156PathMather_WithGlobExpressions {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get(".");
-        Files.walkFileTree(path, new ListAllFilesEndsWithJavaOrClass()); // ListAllFiles vem da aula Aula154SimpleFileVisitor_interacaoAllRecursivaEmDirs
+        Path arquivoTxtEmJavaPath = Paths.get("src/nio/aula156PathMather_WithGlobExpressions.txt");
+        Path arquivoTxtEmJavaPathOrganizado = Paths.get("src/nio/aula156PathMather_WithGlobExpressions_Organizado.txt");
+        if (Files.notExists(arquivoTxtEmJavaPath)){
+            Files.createFile(arquivoTxtEmJavaPath);
+
+            String globExpression = "glob:**/Aula*.{java}";
+            Files.walkFileTree(path, new ListAllFilesEndsWithJavaOrClass(arquivoTxtEmJavaPath, globExpression)); // ListAllFiles vem da aula Aula154SimpleFileVisitor_interacaoAllRecursivaEmDirs
+        }
+
+        try {
+            List<String> lines = Files.readAllLines(arquivoTxtEmJavaPath);
+            String[] array = lines.toArray(new String[lines.size()]);
+            Arrays.sort(array);
+            Files.write(arquivoTxtEmJavaPathOrganizado, Arrays.asList(array));
+        } catch (IOException e) {
+            System.out.println("Erro ao ler/gravar arquivo: " + e.getMessage());
+        }
 
     }
 }
@@ -18,10 +36,27 @@ public class Aula156PathMather_WithGlobExpressions {
 class ListAllFilesEndsWithJavaOrClass extends SimpleFileVisitor<Path> {
     // Atalho para sobre-escrever métodos: CTRL + O
 
+    private final Path outputPath;
+    private final String globExpression;
+    public ListAllFilesEndsWithJavaOrClass(Path saveOutputPath, String globExpression) {
+        super();
+        this.outputPath = saveOutputPath;
+        this.globExpression = globExpression;
+    }
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (isMatcherInPathsWithGlob(file, "glob:**/*.{java,class}")) {
+
+        // Verifica se possuem a extensão .java ou .class
+//        if (isMatcherInPathsWithGlob(file, "glob:**/*.{java,class}")) {
+//            System.out.println("Path que deu match: "+file.getFileName());
+//        }
+
+        // Verifica se possue "Aula" no nome do arquivo:
+
+        if (isMatcherInPathsWithGlob(file, globExpression)) {
             System.out.println("Path que deu match: "+file.getFileName());
+            Files.write(outputPath, (file.getFileName().toString()+"\n").getBytes(), StandardOpenOption.APPEND);
         }
 
         return FileVisitResult.CONTINUE;
