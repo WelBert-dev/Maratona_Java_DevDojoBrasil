67c18fa0db870ad59287a2858571f783cd67ebdc
WelBert-dev <wellison.bertelli@hotmail.com>
2023-02-15 20:59:46 -0300

    Exercita listagem de Paths >= (SimpleFileVisitor)

---
+++ src/nio/SimpleFileVisitor_PathMatcher_FindAllArchivesMore1GBInDirectory.java
@@ -0,0 +1,80 @@
+package nio;
+
+// Buscando arquivos maiores que 1GB em um diretório de maneira recursiva
+// ou seja level ALL abaixo da arvore de diretórios.
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class SimpleFileVisitor_PathMatcher_FindAllArchivesMore1GBInDirectory {
+    public static void main(String[] args) throws IOException {
+
+        Path startDir = Paths.get( "/home/welbert/Downloads");
+        Path outputSaveReturnPath = Paths.get("src/nio/SimpleFileVisitor_listaArquivosMaioresQue1GB.txt");
+
+        if (Files.notExists(outputSaveReturnPath)) {
+            Files.createFile(outputSaveReturnPath);
+
+            LargeFileFinder finder = new LargeFileFinder();
+            Files.walkFileTree(startDir, finder);
+        }
+
+        LargeFileFinder finder = new LargeFileFinder();
+        Files.walkFileTree(startDir.normalize(), finder);
+
+    }
+}
+
+class LargeFileFinder extends SimpleFileVisitor<Path> {
+    private PathMatcher matcher;
+    private String globExpression;
+    private long maxSize = 1_000_000_000L;
+
+    private Path outputFile = Paths.get("src/nio/SimpleFileVisitor_listaArquivosMaioresQue1GB.txt");
+
+    public LargeFileFinder() throws IOException {
+        this.globExpression = "glob:**/*.{mp4,avi,mkv,flv,mov,mpg,wmv}";
+
+        if (Files.notExists(outputFile)) {
+
+            Files.createFile(outputFile);
+        }
+
+        this.matcher = FileSystems.getDefault().getPathMatcher(globExpression);
+
+    }
+
+    public LargeFileFinder(String globExpression) throws IOException {
+        this();
+        this.matcher = FileSystems.getDefault().getPathMatcher(globExpression);
+    }
+
+    public LargeFileFinder(long maxSize) throws IOException {
+        this();
+        this.maxSize = maxSize;
+    }
+
+    public LargeFileFinder(String globExpression, long maxSize) throws IOException {
+        this();
+        this.globExpression = globExpression;
+        this.maxSize = maxSize;
+        this.matcher = FileSystems.getDefault().getPathMatcher(globExpression);
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        try {
+            // verifica se o arquivo é maior que maxSize e se corresponde à expressão glob
+            if (attrs.isRegularFile() && attrs.size() > maxSize &&
+                    file.getFileName().toString().matches(globExpression)) {
+                String output = file.getFileName() + "\n";
+                Files.writeString(outputFile, output, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+}
---
+++ src/nio/SimpleFileVisitor_PathMatcher_FindAllArchivesMore1GBInDirectory_refatoradoPeloChatGPT.java
@@ -0,0 +1,63 @@
+package nio;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class SimpleFileVisitor_PathMatcher_FindAllArchivesMore1GBInDirectory_refatoradoPeloChatGPT {
+    public static void main(String[] args) throws IOException {
+        Path startDir = Paths.get("/home/welbert/Downloads");
+        Path outputSaveReturnPath = Paths.get("src/nio/SimpleFileVisitor_listaArquivosMaioresQue1GB_refactor.txt");
+
+        if (Files.notExists(outputSaveReturnPath)) {
+            Files.createFile(outputSaveReturnPath);
+        }
+
+        LargeFileFinderRefactor finder = new LargeFileFinderRefactor(2_000_000_000L, "glob:**/*.{mp4,avi,mkv,flv,mov,mpg,wmv}", outputSaveReturnPath);
+        Files.walkFileTree(startDir.normalize(), finder);
+    }
+}
+
+class LargeFileFinderRefactor extends SimpleFileVisitor<Path> {
+    private final PathMatcher matcher;
+    private final Path outputFile;
+    private final long maxSize;
+
+    private final String globExpression;
+
+    public LargeFileFinderRefactor(long maxSize, String globExpression, Path outputFile) throws IOException {
+        super();
+        this.maxSize = maxSize;
+        this.globExpression = globExpression;
+        this.outputFile = outputFile;
+
+        if (Files.notExists(outputFile)) {
+            Files.createFile(outputFile);
+        }
+
+        this.matcher = FileSystems.getDefault().getPathMatcher(globExpression);
+    }
+
+    public LargeFileFinderRefactor(String globExpression, long maxSize, Path outputFile) throws IOException {
+        super();
+        this.maxSize = maxSize;
+        this.globExpression = globExpression;
+        this.outputFile = outputFile;
+
+        if (Files.notExists(outputFile)) {
+            Files.createFile(outputFile);
+        }
+
+        this.matcher = FileSystems.getDefault().getPathMatcher(globExpression);
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (attrs.isRegularFile() && attrs.size() > maxSize && matcher.matches(file)) {
+            String output = file.getFileName() + "\n";
+            Files.writeString(outputFile, output, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+}
