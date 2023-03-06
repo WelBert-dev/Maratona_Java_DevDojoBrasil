ddf3d98f8f773d761ca20c7fc6422cf560e04e66
WelBert-dev <wellison.bertelli@hotmail.com>
2023-03-06 15:47:05 -0300

    introd a interface List prog orientada a interface

---
+++ src/colections/Aula166a168List.java
@@ -0,0 +1,22 @@
+package colections;
+
+// - List é uma interface que extends de Collection.
+// - Utilizada para se trabalhar orientado a interface.
+// - Contém os métodos mais genéricos e basicos para se trabalhar com listas.
+// - O tamanho do array aumenta conforme necessário (dinamicamente),
+// por debaixo dos panos é re-criado o array e a variavel de referência passa a
+// apontar para este novo array criado.
+
+// A interface List é uma das interfaces mais importantes do framework de coleções do Java.
+// Ela define um contrato para classes que representam sequências ordenadas de elementos e
+// oferece uma série de métodos para manipulação dessas sequências.
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula166a168List {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>(); // Se n utilizar generics <> ele define como Object. (ou seja qualquer valor)
+        lista.add("Wellison");
+        lista.add("Bertelli");
+    }
+}
