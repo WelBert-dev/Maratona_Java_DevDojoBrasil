03b0f309f6e9d254128c2fc1b5718e9bba8d5321
WelBert-dev <wellison.bertelli@hotmail.com>
2023-04-19 15:46:29 -0300

    add + infos sobre Future e outras classes parecids

--- src/R_concurrent/Aula239Concurrent_Executors_Future_currentThreadAguardaRetornoDoCallable.java
+++ src/R_concurrent/Aula239Concurrent_Executors_Future_currentThreadAguardaRetornoDoCallable.java
@@ -15,6 +15,7 @@ Em termos simples, um Future é uma promessa de que um valor será retornado em
 algum momento futuro. Ele pode ser usado para executar operações em segundo
 plano e retornar um resultado posteriormente.
 
+
 ---> Algumas aplicações comuns do Future incluem:
 
     - Execução de operações assíncronas: Pode ser usado para executar operações em
@@ -41,6 +42,90 @@ Então, quando o resultado estiver disponível, ele pode ser recuperado chamando
 método get() na instância do Future. Se o resultado ainda não estiver disponível,
 a thread current que está chamando o método get() aguardará até que a tarefa seja
 concluída e o resultado esteja disponível.
+
+
+---> Métodos presentes na interface:
+
+- boolean cancel(boolean mayInterruptIfRunning): É usado para cancelar a tarefa
+associada à instância de Future. O parâmetro boolean "mayInterruptIfRunning"
+indica se a thread em execução deve ser interrompida. Se a tarefa já foi concluída,
+o método retorna false.
+
+- boolean isCancelled(): É usado para verificar se a tarefa associada à instância
+de Future foi cancelada. Se a tarefa foi cancelada, o método retorna true.
+
+- boolean isDone(): É usado para verificar se a tarefa associada à instância de
+Future foi concluída. Se a tarefa foi concluída, o método retorna true.
+
+- V get(): É usado para obter o resultado da tarefa associada à instância de
+Future. Se a tarefa ainda não foi concluída, o método bloqueia a current thread
+que chamou o get() até que o resultado esteja disponível. O tipo de retorno do
+método é definido pelo tipo genérico da interface Future.
+
+- V get(long timeout, TimeUnit unit): É usado para obter o resultado da tarefa
+associada à instância de Future com um tempo limite especificado. Se a tarefa
+ainda não foi concluída, o método bloqueia a current thread que esta chamando
+esse método, até que o resultado esteja disponível ou o tempo limite seja atingido.
+O parâmetro "timeout" especifica o tempo limite e o parâmetro "unit" especifica a
+unidade de tempo.
+
+Esses métodos são comuns a todas as classes que implementam a interface Future.
+Os métodos específicos de cada classe, como métodos para encadear operações
+assíncronas em CompletableFuture, são adicionados para fornecer recursos
+adicionais para lidar com resultados assíncronos.
+
+
+---> Mais classes e Interfaces com mesmo propósito, porém com funções adicionais:
+
+* CompletableFuture: É a classe mais avançada e versátil que implementa a interface
+Future. Ela fornece muitos métodos úteis para trabalhar com resultados assíncronos,
+como encadear várias operações assíncronas e manipular erros de forma eficiente.
+O CompletableFuture também permite que você especifique uma execução em segundo
+plano com um Executor personalizado epara operações de longa duração, incluindo
+chamadas de rede, processamento de arquivos grandes e outras tarefas assíncronas.
+
+* FutureTask: É uma classe concreta que implementa a interface Future e é adequada
+para tarefas que precisam ser executadas em segundo plano. Você pode passar uma
+instância de FutureTask para uma instância do ExecutorService e a tarefa será
+executada em uma thread separada. FutureTask é uma opção mais simples que o
+CompletableFuture e fornece apenas o básico para executar uma tarefa em segundo
+plano.
+
+* ScheduledFuture: É uma subinterface da interface Future e é usada para agendar
+tarefas para serem executadas em algum momento futuro. Você pode usar uma instância
+de ScheduledFuture para agendar tarefas periódicas ou para agendar uma tarefa para
+ser executada após um determinado período de tempo.
+
+
+---> Diferenças entre o ScheduledFuture e o ScheduledExecutorService:
+
+O ScheduledExecutorService é uma interface que estende a interface ExecutorService
+e define um conjunto de métodos para agendar tarefas para serem executadas em um
+momento futuro, incluindo tarefas periódicas ou para serem executadas após um
+determinado período de tempo.
+
+O ScheduledFuture, por sua vez, é uma subinterface de Future que estende a interface
+Delayed. Ele representa uma tarefa agendada para ser executada em um momento futuro
+pelo ScheduledExecutorService.
+
+Em resumo, o ScheduledExecutorService é usado para criar e gerenciar agendamentos
+de tarefas e o ScheduledFuture é usado para representar uma tarefa agendada e
+fornecer métodos adicionais para trabalhar com o tempo restante até a execução
+da tarefa.
+
+Os métodos adicionais fornecidos pela interface ScheduledFuture incluem:
+
+- long getDelay(TimeUnit unit): É usado para obter o tempo restante até a execução
+da tarefa agendada em uma unidade de tempo específica.
+
+- int compareTo(Delayed other): É usado para comparar o tempo restante até a execução
+da tarefa agendada com outro objeto Delayed. Se o tempo restante for menor que o do
+outro objeto, este método retorna um valor negativo. Se o tempo restante for maior
+que o do outro objeto, este método retorna um valor positivo. Se o tempo restante for
+igual ao do outro objeto, este método retorna zero.
+
+Ambas as interfaces, ScheduledExecutorService e ScheduledFuture, são usadas em
+conjunto para criar e gerenciar tarefas agendadas.
 
 */
 
