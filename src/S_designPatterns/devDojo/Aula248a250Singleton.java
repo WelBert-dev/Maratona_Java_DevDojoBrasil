c04a36cb5caf629e5b6da9837efad0996321b770
WelBert-dev <wellison.bertelli@hotmail.com>
2023-05-02 16:05:58 -0300

    add aula250 Singleton p03: Utilizando Enum (Por padrão é Thread-safe e não é possível burlar com uso de reflexão)

--- src/S_designPatterns/devDojo/Aula248a250Singleton.java
+++ src/S_designPatterns/devDojo/Aula248a250Singleton.java
@@ -1,6 +1,54 @@
 package S_designPatterns.devDojo;
 
+/* Existem 3 formas de implementar esse padrão sendo elas:
+
+- Eager Initialization: Cria uma classe que já instância a criação do objeto no
+momento em que o classLoader carrega a classe em memória, e compartilha o mesmo
+objeto em memória para todos que precisarem.
+
+    - Obs: Eager pois ele carrega de uma forma ansiosa/gulosa ou seja, já cria a
+    instância compartilhada no momento em que a classe é carregada pela JVM.
+
+    - Mais Thread-Safe que o de baixo pois não ocorrem verificações se já
+    existe uma instância do objeto criado em memória no momento em que precisa da
+    instância singleton compartilhada (No momento em que se chama o objeto em questão).
+
+    - Porém podemos ainda sim burlar o singleton (Compartilhamento da mesma instância
+    do objeto em memória) utilizando reflexão para alterar a acessibilidade do
+    construtor privado.
+
+- Lazy Initialization: Cria uma classe que só instância a criação do objeto
+quando alguem precisa/chama, e apartir dai compartilha esse mesmo objeto
+em memória para as próximas chamadas.
+
+    - Obs: Lazy porisso, ele não carrega ansioso/guloso, ele só cria quando
+    é necessário, e apartir dai compartilha essa mesma criação para todos
+    que precisarem/chamarem ele.
+
+    - Não Thread-Safe pois é necessário realizar verificações se o objeto
+    já existe em memória para ai sim criar, isso pode ocorrer condições de
+    corrida em ambientes de paralelismo.
+
+    - Para solucionar podemos sincronizar essa verificação e outros métodos
+    que são propícios a problemas de corrida.
+
+    - Apresenta o mesmo problama anterior sobre ser possível burlar com uso
+    de reflexão.
+
+- Utilizando Enum: Ao invés de utilizar classes para implementar esse padrão
+podemos utilizar enumeradores que por padrão já são Thread-Safe para a criação/
+instanciação de Objetos e também é seguro em relação a burlações com uso de
+reflexão.
+
+    - Por Padrão é Thread-Safe na instânciação/criação do Objeto.
+    - Por Padrão não é possível alterar o modificador de acesso do construtor
+    utilizando reflexão.
+
+
+*/
+
 import S_designPatterns.domain.AircraftSingletonEager;
+import S_designPatterns.domain.AircraftSingletonEnum;
 import S_designPatterns.domain.AircraftSingletonLazy;
 
 import java.lang.reflect.Constructor;
@@ -60,5 +108,20 @@ public class Aula248a250Singleton {
         // Erá pra ser false pois já retiramos esse assento do objeto singleton original, porém não é o mesmo obj
         // pois burlamos o singleton com reflexão e agr é outro obj em memória logo == true
 
+        // -------------- [Solução para a burlação com reflexão anterior] ------------------
+        // Utilizando Enum: - Por padrão a criação de enumerações é Thread-Safe!
+        //                  - Por padrão não é possível alterar o modificador de acesso com reflexão.
+
+        AircraftSingletonEnum instanceEnum = AircraftSingletonEnum.INSTANCE;
+        AircraftSingletonEnum instanceEnum2 = AircraftSingletonEnum.INSTANCE;
+
+        System.out.println(instanceEnum.bookSeat("1A")); // true
+
+        System.out.println(instanceEnum2.bookSeat("1A")); // false
+
+        // Provando que ambas referências apontam para o mesmo obj em memória:
+        System.out.println(instanceEnum.hashCode()); // 1096979270
+        System.out.println(instanceEnum2.hashCode()); // 1096979270
+        System.out.println(instanceEnum == instanceEnum2); // true
     }
 }
---
+++ src/S_designPatterns/domain/AircraftSingletonEnum.java
@@ -0,0 +1,18 @@
+package S_designPatterns.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum AircraftSingletonEnum {
+    INSTANCE;
+    private final Set<String> availableSeats;
+
+    AircraftSingletonEnum() {
+        this.availableSeats = new HashSet<>();
+        this.availableSeats.add("1A");
+        this.availableSeats.add("1B");
+    }
+    public boolean bookSeat(String seat) {
+        return availableSeats.remove(seat);
+    }
+}
