528e7cf58a80921c9d21c9904411e61d276c272c
WelBert-dev <wellison.bertelli@hotmail.com>
2023-05-02 20:38:01 -0300

    Add maven ao projeto, altera os diretórios porisso a modificação em massa

---
+++ src/main/java/T_jdbc/conn/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package T_jdbc.conn;
+
+/* API para manipulação de db: java.sql
+
+- Principais classes e interfaces:
+   - Connection:
+   - Statement:
+   - ResultSet:
+   - DriverManager:
+
+- Essas interfaces definem os contratos em que os diferentes bancos devem fornecer
+as implementações necessárias, assim podemos trabalhar orientado a interface e
+desaclopar essas dependencias facilmente, podendo assim realizar trocas de bancos
+facilmente.
+
+- Podemos adicionar os drivers manualmente ou utilizando gerenciadores de dependências
+como o Maven (para facilitar a nossa vida).
+    - mysql connector java maven;
+
+*/
+
+
+
+public class ConnectionFactory {
+
+}
